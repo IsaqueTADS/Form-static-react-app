e03805df9df76e6aabc7a154c8946c85f48a12a4
IsaqueTADS <isaqueofc18@gmail.com>
2025-03-04 13:34:42 -0300

    css no react n presta vey

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Form from "./Components/Form";
 const App = () => {
   return <div>
   
-  <h1>Entre em contato</h1>
   <Form />
 
 
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,44 +1,49 @@
 import { useState } from "react";
 
-
-import React from 'react'
+import React from "react";
 
 const Form = () => {
- 
-    const [dadosFormulario, setDadosFormulario] = useState({
-      nome: "",
-      email: "",
-      mensagem: "",
+  const [dadosFormulario, setDadosFormulario] = useState({
+    nome: "",
+    email: "",
+    mensagem: "",
+  });
+
+  const lidarComMudanca = (evento) => {
+    const { name, value } = evento.target;
+    setDadosFormulario({ ...dadosFormulario, [name]: value });
+  };
+
+  const lidarComEnvio = async (evento) => {
+    evento.preventDefault();
+    const chaveApi = import.meta.env.VITE_STATIC_FORMS_API_KEY; // Pegando a chave do .env
+
+    const resposta = await fetch("https://api.staticforms.xyz/submit", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        accessKey: chaveApi,
+        ...dadosFormulario,
+      }),
     });
-  
-    const lidarComMudanca = (evento) => {
-      const { name, value } = evento.target;
-      setDadosFormulario({ ...dadosFormulario, [name]: value });
-    };
-  
-    const lidarComEnvio = async (evento) => {
-      evento.preventDefault();
-      const chaveApi = import.meta.env.VITE_STATIC_FORMS_API_KEY; // Pegando a chave do .env
-  
-      const resposta = await fetch("https://api.staticforms.xyz/submit", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          accessKey: chaveApi,
-          ...dadosFormulario,
-        }),
-      });
-  
-      if (resposta.ok) {
-        alert("Mensagem enviada com sucesso!");
-        setDadosFormulario({ nome: "", email: "", mensagem: "" }); // Limpa os campos após o envio
-      } else {
-        alert("Houve um erro ao enviar a mensagem.");
-      }
-    };
-  
-    return (
+
+    if (resposta.ok) {
+      alert("Mensagem enviada com sucesso!");
+      setDadosFormulario({ nome: "", email: "", mensagem: "" }); // Limpa os campos após o envio
+    } else {
+      alert("Houve um erro ao enviar a mensagem.");
+    }
+  };
+
+  return (
+    <div className="paiForm">
+      <div>
+        <img src="./public/img/animate.svg" alt="" />
+      </div>
+
       <form onSubmit={lidarComEnvio} className="formulario-container">
+        <h1>Entre em contato</h1>
+
         <label>
           Nome:
           <input
@@ -49,7 +54,6 @@ const Form = () => {
             required
           />
         </label>
-  
         <label>
           E-mail:
           <input
@@ -60,7 +64,6 @@ const Form = () => {
             required
           />
         </label>
-  
         <label>
           Mensagem:
           <textarea
@@ -70,11 +73,10 @@ const Form = () => {
             required
           />
         </label>
-  
         <button type="submit">Enviar</button>
       </form>
-    );
-  };
-  
+    </div>
+  );
+};
 
-export default Form
+export default Form;
