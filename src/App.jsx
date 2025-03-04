066e48c22e58f8b7bbe1d2c2a2c7fcd600bcf2b9
IsaqueTADS <isaqueofc18@gmail.com>
2025-03-04 02:54:12 -0300

    projeto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Form from "./Components/Form";
+
+const App = () => {
+  return <div>
+  
+  <h1>Entre em contato</h1>
+  <Form />
+
+
+  </div>;
+};
+
+export default App;
---
+++ src/Components/Form.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+
+
+import React from 'react'
+
+const Form = () => {
+ 
+    const [dadosFormulario, setDadosFormulario] = useState({
+      nome: "",
+      email: "",
+      mensagem: "",
+    });
+  
+    const lidarComMudanca = (evento) => {
+      const { name, value } = evento.target;
+      setDadosFormulario({ ...dadosFormulario, [name]: value });
+    };
+  
+    const lidarComEnvio = async (evento) => {
+      evento.preventDefault();
+      const chaveApi = import.meta.env.VITE_STATIC_FORMS_API_KEY; // Pegando a chave do .env
+  
+      const resposta = await fetch("https://api.staticforms.xyz/submit", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          accessKey: chaveApi,
+          ...dadosFormulario,
+        }),
+      });
+  
+      if (resposta.ok) {
+        alert("Mensagem enviada com sucesso!");
+        setDadosFormulario({ nome: "", email: "", mensagem: "" }); // Limpa os campos após o envio
+      } else {
+        alert("Houve um erro ao enviar a mensagem.");
+      }
+    };
+  
+    return (
+      <form onSubmit={lidarComEnvio} className="formulario-container">
+        <label>
+          Nome:
+          <input
+            type="text"
+            name="nome"
+            value={dadosFormulario.nome}
+            onChange={lidarComMudanca}
+            required
+          />
+        </label>
+  
+        <label>
+          E-mail:
+          <input
+            type="email"
+            name="email"
+            value={dadosFormulario.email}
+            onChange={lidarComMudanca}
+            required
+          />
+        </label>
+  
+        <label>
+          Mensagem:
+          <textarea
+            name="mensagem"
+            value={dadosFormulario.mensagem}
+            onChange={lidarComMudanca}
+            required
+          />
+        </label>
+  
+        <button type="submit">Enviar</button>
+      </form>
+    );
+  };
+  
+
+export default Form
